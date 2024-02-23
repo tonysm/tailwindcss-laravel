8cffabb6ab455cbfabde7e8721e3cc279f3995ab
Tony Messias <tonysm@hey.com>
2024-02-23 00:48:12 -0300

    tweaks regex

--- src/Actions/AppendTailwindTag.php
+++ src/Actions/AppendTailwindTag.php
@@ -8,7 +8,7 @@ class AppendTailwindTag
     {
         return preg_replace(
             '/(\s*)(<\/head>)/',
-            "\n\\1    <!-- TailwindCSS Styles -->".
+            PHP_EOL."\\1    <!-- TailwindCSS Styles -->".
             "\\1    <link rel=\"stylesheet\" href=\"{{ tailwindcss('css/app.css') }}\" />\\1\\2",
             $contents,
         );
